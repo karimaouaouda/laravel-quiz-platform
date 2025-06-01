dab67193bb13583c6ba0bd081130f50d6b1eb6ad
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-06-01 22:28:38 +0100

    update the project

--- app/Filament/Student/Resources/SubmissionResource.php
+++ app/Filament/Student/Resources/SubmissionResource.php
@@ -8,9 +8,11 @@ use App\Models\Submission;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Support\Colors\Color;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class SubmissionResource extends Resource
@@ -18,6 +20,21 @@ class SubmissionResource extends Resource
     protected static ?string $model = Submission::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
+
+    public static function canDelete(Model $record): bool
+    {
+        return false;
+    }
+
+    public static function canEdit(Model $record): bool
+    {
+        return false;
+    }
 
     public static function form(Form $form): Form
     {
@@ -30,8 +47,40 @@ class SubmissionResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
+            ->heading("Your submission to quizzes")
+            ->description('here you will track your submissions')
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('id')
+                    ->prefix("#")
+                    ->badge()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('quiz.teacher.name')
+                    ->color(Color::Indigo)
+                    ->badge(),
+                Tables\Columns\TextColumn::make('quiz.title')
+                    ->searchable()
+                    ->words(5)
+                    ->tooltip(fn($state) => $state)
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('quiz.subject.name')
+                    ->badge()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('score')
+                    ->suffix('%')
+                    ->badge()
+                    ->color(function($state){
+                        if( $state < 50 )
+                            return Color::Red;
+                        if ($state < 75)
+                            return Color::Green;
+                        return Color::Blue;
+                    })
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('submit at')
+                    ->dateTime()
+                    ->badge()
+                    ->sortable(),
             ])
             ->filters([
                 //
